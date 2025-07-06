bcd60bdd99e3c2159b26384d4e95de5accf41b66
CharlieJ2005 <163343248+CharlieJ2005@users.noreply.github.com>
2025-07-06 14:21:02 +0200

    fixed initialisation

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,14 +1,16 @@
 class HTMLNode():
     def __init__(self, tag=None, value=None, children=None, props=None):
-        self.tag = None
-        self.value = None
-        self.children = None
-        self.props = None
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
 
     def to_html(self):
         raise NotImplementedError
 
     def props_to_html(self):
+        if not self.props:
+            return ""
         html_string = ""
         for key, value in self.props.items():
             html_string += f'{key}="{value}" '
